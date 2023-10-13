8297f53c06ec86aa7eda8d5ee43f88980fee4730
Natalia <nata.guarnizo@gmail.com>
2023-10-13 13:28:59 +0200

    struktur changes

--- PaperlessREST/PaperlessREST/Program.cs
+++ PaperlessREST/PaperlessREST/Program.cs
@@ -26,7 +26,8 @@ namespace Org.OpenAPITools
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                   webBuilder.UseStartup<Startup>();
+                   webBuilder.UseStartup<Startup>()
+                             .UseUrls("http://0.0.0.0:8080/");
                 });
     }
 }
